36ec9dda5c6a9abff82271f46a972eb2a63c2cd2
zhabibi-z <zhabibi@neiu.edu>
2025-01-10 21:43:06 -0600

    hero_banner

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './Home.css'
 import Navbar from '../../components/Navbar/Navbar'
-import hero_banner from '../../assets/hero_banner.jpg'
+import hero_banner from './hero_banner.jpg'
 import hero_title from '../../assets/hero_title.png'
 import play_icon from '../../assets/play_icon.png'
 import info_icon from '../../assets/info_icon.png'
